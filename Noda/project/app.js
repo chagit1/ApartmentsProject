ad0016a8cc1bf0930e53fa24e098aff56a35d418
chagit1 <chagit0548559840@gmail.com>
2024-03-31 23:54:44 +0300

    add_Dockerfile

--- Noda/project/app.js
+++ Noda/project/app.js
@@ -1,53 +1,41 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const ApartmentRouter = require('./api/Routes/apartment')
-const CategoryRouter = require('./api/Routes/category')
-const CityRouter = require('./api/Routes/city')
-const ClientRouter = require('./api/Routes/client')
-const PublisherRouter = require('./api/Routes/publisher')
-const cors = require('cors')
-const app = express()
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-dotenv.config()
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// app.use(function(req,res,next){
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-Wit, Content-Type, Accept")
-//     next()
-//     });
+var app = express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-app.options('*', cors());
-app.use(bodyParser.json());
-app.use(cors());
 
-app.use('/uploads',express.static('uploads'))
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-mongoose.connect(process.env.URI, {})
-    .then(() => {
-        console.log(`connection to mongoDb succeed!`);
-    })
-    .catch(error => {
-        console.log({ error });
-    })
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.get('/', (req, res) => {
-    res.status(200).send('✌️✌️✌️')
-})
-
-app.use('/apartment', ApartmentRouter)
-app.use('/category', CategoryRouter)
-app.use('/city', CityRouter)
-app.use('/client', ClientRouter)
-app.use('/publisher', PublisherRouter)
-
-app.listen(3001, () => {
-    console.log(`app in http//:localhost:3001`);
-})
-
+module.exports = app;
